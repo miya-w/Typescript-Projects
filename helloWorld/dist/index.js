490372ac01a641ed29a4af59746f5ea471951831
miya-w <62531644+hellomiyawu@users.noreply.github.com>
2022-11-17 16:02:43 +1100

    update README

--- helloWorld/dist/index.js
+++ helloWorld/dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
-console.log('helloworld');
 let age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+let user = [1, 'John'];
+user.push(1);
+function helloworld(city) {
+    return 'hello,' + city;
+}
+let city = 'Melbourne';
+console.log(helloworld(city));
 //# sourceMappingURL=index.js.map
--- helloWorld/src/index.ts
+++ helloWorld/src/index.ts
@@ -7,9 +7,16 @@ console.log(age);
 let user: [number, string] = [1,'John']
 user.push(1);
 
+
 //age = "20"
 //Type 'string' is not assignable to type 'number'.ts(2322)
 
+function helloworld(city: string){
+    return 'hello,' + city;
+}
+let city = 'Melbourne';
+console.log(helloworld(city));
 
 
-//enums
+
+
