fe79b311bb32c085a68ea82daf3fbee569e3ee4b
miya-w <62531644+hellomiyawu@users.noreply.github.com>
2022-11-17 14:24:22 +1100

    upadte helloWorld

---
+++ helloWorld/src/index.ts
@@ -0,0 +1,15 @@
+// console.log('helloworld');
+let age: number = 20;
+if (age<50)
+age += 10;
+console.log(age);
+
+let user: [number, string] = [1,'John']
+user.push(1);
+
+//age = "20"
+//Type 'string' is not assignable to type 'number'.ts(2322)
+
+
+
+//enums
